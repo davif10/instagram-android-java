e79d53e6facb18177340242a05bd2df98f8258f9
Davi Francisco da Silva <43782019+davif10@users.noreply.github.com>
2021-01-03 21:50:24 -0300

    Criando interface de Cadastro.

---
+++ Instagram/app/src/main/java/com/daviprojetos/instagram/activity/CadastroActivity.java
@@ -0,0 +1,16 @@
+package com.daviprojetos.instagram.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.daviprojetos.instagram.R;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
--- Instagram/app/src/main/java/com/daviprojetos/instagram/activity/LoginActivity.java
+++ Instagram/app/src/main/java/com/daviprojetos/instagram/activity/LoginActivity.java
@@ -2,7 +2,9 @@ package com.daviprojetos.instagram.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.daviprojetos.instagram.R;
 
@@ -12,5 +14,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void abrirCadastro(View view){
+        Intent i = new Intent(LoginActivity.this,CadastroActivity.class);
+        startActivity(i);
     }
 }
