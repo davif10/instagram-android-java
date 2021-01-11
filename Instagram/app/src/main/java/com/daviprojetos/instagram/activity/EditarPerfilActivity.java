49922d2f36b10ab478805a3f9453a18a6994c56f
Davi Francisco da Silva <43782019+davif10@users.noreply.github.com>
2021-01-10 22:58:19 -0300

    Alterando nome e customizando botão voltar. -Alterando imagem de perfil

--- Instagram/app/src/main/java/com/daviprojetos/instagram/activity/EditarPerfilActivity.java
+++ Instagram/app/src/main/java/com/daviprojetos/instagram/activity/EditarPerfilActivity.java
@@ -1,19 +1,32 @@
 package com.daviprojetos.instagram.activity;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.daviprojetos.instagram.R;
+import com.daviprojetos.instagram.helper.ConfiguracaoFirebase;
 import com.daviprojetos.instagram.helper.UsuarioFirebase;
 import com.daviprojetos.instagram.model.Usuario;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -23,6 +36,8 @@ public class EditarPerfilActivity extends AppCompatActivity {
     private TextInputEditText editNomePerfil, editEmailPerfil;
     private Button buttonSalvarAlteracoes;
     private Usuario usuarioLogado;
+    private static final  int SELECAO_GALERIA = 200;
+    private StorageReference storageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +46,7 @@ public class EditarPerfilActivity extends AppCompatActivity {
 
         //Configurações iniciais
         usuarioLogado = UsuarioFirebase.getDadosUsuarioLogado();
+        storageRef = ConfiguracaoFirebase.getFirebaseStorage();
 
         //Configurar toolbar
         Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
@@ -64,6 +80,69 @@ public class EditarPerfilActivity extends AppCompatActivity {
             }
         });
 
+        //Alterar foto do usuário
+        textAlterarFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                if(i.resolveActivity(getPackageManager()) != null){
+                    startActivityForResult(i,SELECAO_GALERIA);
+                }
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            Bitmap imagem = null;
+
+            try{
+                //Seleção apenas da galeria
+                switch (requestCode){
+                    case SELECAO_GALERIA:
+                        Uri localImagemSelecionada = data.getData();
+                        imagem = MediaStore.Images.Media.getBitmap(getContentResolver(), localImagemSelecionada);
+                        break;
+                }
+
+                //Caso tenha sido escolhido uma imagem
+                if(imagem != null){
+                    //Configura a imagem na tela
+                    imageEditarPerfil.setImageBitmap(imagem);
+
+                    //Recuperar dados da imagem para o firebase
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    imagem.compress(Bitmap.CompressFormat.JPEG,70,baos);
+                    byte[] dadosImagem = baos.toByteArray();
+
+                    //Salvar imagem no firebase
+                    StorageReference imagemRef = storageRef
+                            .child("imagens")
+                            .child("perfil")
+                            .child("<id-usuario>.jpeg");
+
+                    UploadTask uploadTask = imagemRef.putBytes(dadosImagem);
+                    uploadTask.addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(EditarPerfilActivity.this, "Erro ao fazer upload da imagem!", Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Toast.makeText(EditarPerfilActivity.this, "Sucesso ao fazer upload da imagem!", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 
     public void inicializarComponentes(){
--- Instagram/app/src/main/java/com/daviprojetos/instagram/helper/ConfiguracaoFirebase.java
+++ Instagram/app/src/main/java/com/daviprojetos/instagram/helper/ConfiguracaoFirebase.java
@@ -3,10 +3,13 @@ package com.daviprojetos.instagram.helper;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class ConfiguracaoFirebase {
     private static DatabaseReference referenciaFirebase;
     private static FirebaseAuth referenciaAutenticacao;
+    private static StorageReference storage;
 
     //retorna a instância do FirebaseAuth
     public static FirebaseAuth getFirebaseAutenticacao(){
@@ -22,5 +25,14 @@ public class ConfiguracaoFirebase {
             referenciaFirebase = FirebaseDatabase.getInstance().getReference();
         }
         return referenciaFirebase;
+    }
+
+    //Retorna a instância do FirebaseStorage
+
+    public static StorageReference getFirebaseStorage(){
+        if(storage == null){
+            storage = FirebaseStorage.getInstance().getReference();
+        }
+        return storage;
     }
 }
